ba3c9a273c08719d4ff7634c044b247a10ed2b23
julybro <baro.julia@gmail.com>
2020-02-10 11:26:32 +0100

    creating my first components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-// let's go!
-alert('hey');
+import React from 'react';
+import {render} from 'react-dom'; //importing only the render method 
+import StorePicker from './components/StorePicker'; //relative path
+
+render(<StorePicker />, document.querySelector('#main'));
+
+
