523039d02b7a38bb3e82c4d1863cbd1ef2b38138
julybro <baro.julia@gmail.com>
2020-02-11 09:35:23 +0100

    writing HTML with JSX

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>I am the store picker!</p>
+        return ( //do not forget space and brackets if you return multiple lines
+            <form className="store-selector">
+                { /* this is how you can comment in jsx */ }
+                <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store →</button>
+            </form>
+        )
     }
 }
 
 export default StorePicker;
+
+
+
+
+//import React from 'react';
+
+//class StorePicker extends React.Component {
+//    render() {
+//        return ( //do not forget space and brackets if you return multiple lines
+//        <React.Fragment> {/*use React.Fragment or blank tag <> as wrapper*/}
+//            <p>Fish!</p> {/*you can not return sibiling elements, wrap it in another tag*/}
+//            <form className="store-selector">
+//                <h2>Please Enter A Store</h2>
+//            </form> {/*in jsx use className intead of class*/}
+//        </React.Fragment>
+//        )
+//    }
+//}
+
+//export default StorePicker;
