139b604032dbba3d096615bc5fa87684768c5826
julybro <baro.julia@gmail.com>
2020-02-11 09:48:39 +0100

    loading CSS into my React App

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,9 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return ( //do not forget space and brackets if you return multiple lines
+        return ( 
             <form className="store-selector">
-                { /* this is how you can comment in jsx */ }
                 <h2>Please Enter A Store</h2>
                 <input type="text" required placeholder="Store Name" />
                 <button type="submit">Visit Store →</button>
@@ -17,20 +16,3 @@ export default StorePicker;
 
 
 
-
-//import React from 'react';
-
-//class StorePicker extends React.Component {
-//    render() {
-//        return ( //do not forget space and brackets if you return multiple lines
-//        <React.Fragment> {/*use React.Fragment or blank tag <> as wrapper*/}
-//            <p>Fish!</p> {/*you can not return sibiling elements, wrap it in another tag*/}
-//            <form className="store-selector">
-//                <h2>Please Enter A Store</h2>
-//            </form> {/*in jsx use className intead of class*/}
-//        </React.Fragment>
-//        )
-//    }
-//}
-
-//export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom'; //importing only the render method 
 import StorePicker from './components/StorePicker'; //relative path
+import './css/style.css';
 
 render(<StorePicker />, document.querySelector('#main'));
 
